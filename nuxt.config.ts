a7f862cebc1cafda8f24a9e7e619461af9186a66
parich15 <oscarparicio8@gmail.com>
2025-10-29 20:27:50 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@pinia/nuxt',
+    'reka-ui',
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
+    'nuxt-directus'
+  ]
+})
